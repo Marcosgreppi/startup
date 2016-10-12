97331fb644cb3c71b23bf8f1bb4b9c209707b749
Marcos Greppi <marcos.greppi@globant.com>
2016-10-12 16:52:08 -0300

    Marcos Greppi Topic1 Finished up to point 10

--- 01-JS-DOM-APIs/1-1.js
+++ 01-JS-DOM-APIs/1-1.js
@@ -1,9 +1,9 @@
-function show() {
+function showonload () {
   let obj = document.getElementById("hidden");
   obj.style.display = "block";
 }
 
-function joke() {
+function joke () {
   let xhttp = new XMLHttpRequest();
   xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
   xhttp.onreadystatechange = function(event) {
@@ -18,16 +18,16 @@ function joke() {
     xhttp.send();
 }
 
-function ajaxcall(){
+function ReusableJoke () {
     let config = {
       url: "http://api.icndb.com/jokes/random"
     };
     function resolve(xhttp) {
       let response = JSON.parse(event.target.response);
-      document.getElementById("sectionAjax").innerHTML = response.value.joke;
+      document.getElementById("sectionReusableJoke").innerHTML = response.value.joke;
       if (xhttp.readyState === XMLHttpRequest.DONE) {
         if (xhttp.responseText.value) {
-          document.getElementById("buttonAjax").innerHTML  = xhttp.responseText.value.joke;
+          document.getElementById("buttonReusableJoke").innerHTML  = xhttp.responseText.value.joke;
         }
       }
     };
@@ -37,14 +37,14 @@ function ajaxcall(){
     requestreusable(config).then(resolve,reject);
   };
 
-function requestreusable(object, resolve, reject) {
+function requestreusable (object) {
   return new Promise( function (resolve, reject) {
     let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", object.url , true);
+    xhttp.open("GET", object.url);
     xhttp.send();
     xhttp.onload = function () {
       if (this.status == 200) {
-        resolve(xhttp);
+        resolve(xhttp.response);
       } else {
         reject(this.statusText);
       }
@@ -55,17 +55,46 @@ function requestreusable(object, resolve, reject) {
   })
 }
 
-function Qsearch() {
-  let xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "https://api.github.com/search/repositories", true);
-  xhttp.onreadystatechange = function(event) {
-    let response = JSON.parse(event.target.response);
-    document.getElementById("sectionQSearch").innerHTML = response.value.joke;
-    if (xhttp.readyState === XMLHttpRequest.DONE) {
-      if (xhttp.responseText.value) {
-        document.getElementById("buttonQSearch").innerHTML  = xhttp.responseText.value.joke;
-        }
-      }
-    }
-    xhttp.send();
+function config (methodHttp, url, asyncronic){
+ this.methodHttp = methodHttp;
+ this.url = url;
+ this.asyncronic = asyncronic;
+}
+
+function QSearch () {
+  let objconfig = new config ("GET", "https://api.github.com/search/repositories?q=javascript", true);
+  requestreusable(objconfig).then(function(response) {
+    let list = document.getElementById("list");
+    let items = JSON.parse(response).items;
+
+    items.map(function(item) {
+      var columnNode = document.createElement("li");
+      var textNode = document.createTextNode(item.full_name);
+      columnNode.appendChild(textNode);
+      list.appendChild(columnNode);
+    });
+
+  },
+  function(error) {
+    document.getElementById("hidden").style.color = "red";
+  });
+}
+
+function FreeSearch (query) {
+  let objconfig = new config ("GET", "https://api.github.com/search/repositories?q="+query, true);
+  requestreusable(objconfig).then(function(response) {
+    let list = document.getElementById("list");
+    let items = JSON.parse(response).items;
+
+    items.map(function(item) {
+      var columnNode = document.createElement("li");
+      var textNode = document.createTextNode(item.full_name);
+      columnNode.appendChild(textNode);
+      list.appendChild(columnNode);
+    });
+
+  },
+  function(error) {
+    document.getElementById("hidden").style.color = "red";
+  });
 }
