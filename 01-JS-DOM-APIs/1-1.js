5ae38d636378436ede6a4969e3215a1711a618a2
Marcos Greppi <marcos.greppi@globant.com>
2016-10-17 14:25:08 -0300

    Marcos Greppi Topic1 Fixed everything

--- 01-JS-DOM-APIs/1-1.js
+++ 01-JS-DOM-APIs/1-1.js
@@ -1,45 +1,38 @@
-function showonload () {
-  let obj = document.getElementById("hidden");  
+function showonload() {
+  let obj = document.getElementById("hidden");
   obj.style.display = "block";
 }
 
-function joke () {
+function joke() {
+  let response;
   let xhttp = new XMLHttpRequest();
   xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
   xhttp.onreadystatechange = function(event) {
-    let response = JSON.parse(event.target.response);
+    response = JSON.parse(event.target.response);
     document.getElementById("sectionJoke").innerHTML = response.value.joke;
-    if (xhttp.readyState === XMLHttpRequest.DONE) {
-      if (xhttp.responseText.value) {
-        document.getElementById("buttonJoke").innerHTML  = xhttp.responseText.value.joke;
-        }
-      }
     }
     xhttp.send();
 }
 
-function ReusableJoke () {
-    let config = {
-      url: "http://api.icndb.com/jokes/random"
+function ReusableJoke() {
+  let response;
+  let config = {
+    url: "http://api.icndb.com/jokes/random"
     };
-    function resolve(xhttp) {
-      let response = JSON.parse(event.target.response);
-      document.getElementById("sectionReusableJoke").innerHTML = response.value.joke;
-      if (xhttp.readyState === XMLHttpRequest.DONE) {
-        if (xhttp.responseText.value) {
-          document.getElementById("buttonReusableJoke").innerHTML  = xhttp.responseText.value.joke;
-        }
-      }
-    };
+  function resolve(xhttp) {
+    response = JSON.parse(event.target.response);
+    document.getElementById("sectionReusableJoke").innerHTML = response.value.joke;
+    }
     function reject() {
-       document.getElementById("hidden").style.backgroundColor="red";
+      document.getElementById("hidden").style.backgroundColor="red";
     };
     requestreusable(config).then(resolve,reject);
-  };
+};
 
-function requestreusable (object) {
+function requestreusable(object) {
+  let xhttp;
   return new Promise( function (resolve, reject) {
-    let xhttp = new XMLHttpRequest();
+    xhttp = new XMLHttpRequest();
     xhttp.open("GET", object.url);
     xhttp.send();
     xhttp.onload = function () {
@@ -55,46 +48,54 @@ function requestreusable (object) {
   })
 }
 
-function config (methodHttp, url, asyncronic){
+function config(methodHttp, url, asyncronic){
  this.methodHttp = methodHttp;
  this.url = url;
  this.asyncronic = asyncronic;
 }
 
-function QSearch () {
+function QSearch() {
   let objconfig = new config ("GET", "https://api.github.com/search/repositories?q=javascript", true);
+  let columnNode;
+  let textNode;
+  let list;
+  let items;
   requestreusable(objconfig).then(function(response) {
-    let list = document.getElementById("list");
-    let items = JSON.parse(response).items;
+    list = document.getElementById("list");
+    items = JSON.parse(response).items;
 
-    items.map(function(item) {
-      var columnNode = document.createElement("li");
-      var textNode = document.createTextNode(item.full_name);
+    items.map(function (item) {
+      columnNode = document.createElement("li");
+      textNode = document.createTextNode(item.full_name);
       columnNode.appendChild(textNode);
       list.appendChild(columnNode);
     });
 
   },
-  function(error) {
+  function (error) {
     document.getElementById("hidden").style.color = "red";
   });
 }
 
-function FreeSearch (query) {
+function FreeSearch(query) {
   let objconfig = new config ("GET", "https://api.github.com/search/repositories?q="+query, true);
+  let columnNode;
+  let textNode;
+  let list;
+  let items;
   requestreusable(objconfig).then(function(response) {
-    let list = document.getElementById("list");
-    let items = JSON.parse(response).items;
+    list = document.getElementById("list");
+    items = JSON.parse(response).items;
 
-    items.map(function(item) {
-      var columnNode = document.createElement("li");
-      var textNode = document.createTextNode(item.full_name);
+    items.map(function (item) {
+      columnNode = document.createElement("li");
+      textNode = document.createTextNode(item.full_name);
       columnNode.appendChild(textNode);
       list.appendChild(columnNode);
     });
 
   },
-  function(error) {
+  function (error) {
     document.getElementById("hidden").style.color = "red";
   });
 }
--- 01-JS-DOM-APIs/1-2.js
+++ 01-JS-DOM-APIs/1-2.js
@@ -1,24 +1,29 @@
-function Matrix(){
+function Matrix () {
   matrix = [
     ["Esteban Quito","Ecuador", "North Korea"],
     ["John Smith","Great Britain", "Japan"],
     ["Federico Lopez","Spain", "Australia"],
   ];
-  console.log(matrix[0][0]);
   return matrix;
 }
 
-function importMatrix(matrix) {
-  console.log(matrix);
+function importMatrix (matrix) {
+  let trNode;
+  let tdNode;
+  let dataNode;
+  let indexTR = 0;
+  let indexTD = 0;
 
-  for(var indexTR = 0; indexTR < 3; indexTR++ ){
-    var trNode = document.createElement("tr");
-    for (var indexTD = 0; indexTD < 3; indexTD++) {
-        var tdNode = document.createElement("td");
-        var dataNode = document.createTextNode(matrix[indexTR][indexTD]);
+    for(indexTR; indexTR < 3; indexTR++ ){
+      trNode = document.createElement("tr");
+
+      for (indexTD; indexTD < 3; indexTD++) {
+        tdNode = document.createElement("td");
+        dataNode = document.createTextNode(matrix[indexTR][indexTD]);
         tdNode.appendChild(dataNode);
         trNode.appendChild(tdNode);
-    }
-    document.getElementById("table").appendChild(trNode);
+      }
+
+      document.getElementById("table").appendChild(trNode);
   }
 }
