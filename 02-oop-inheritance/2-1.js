2e314a970173a3249afed199be396bcb575333d3
Marcos Greppi <marcos.greppi@globant.com>
2016-10-24 17:21:39 -0300

    Marcos Greppi Topic2 Added the es6 template strings

--- 02-oop-inheritance/2-1.js
+++ 02-oop-inheritance/2-1.js
@@ -1,32 +1,32 @@
-import {eventEmitter} from "./classEventEmitter";
-import {Logger} from "./classLogger";
-import {movieClass} from "./classMovie";
-import {Actor} from "./classActor";
-import {Social} from "./Social";
+import {eventEmitter} from `./classEventEmitter`;
+import {Logger} from `./classLogger`;
+import {movieClass} from `./classMovie`;
+import {Actor} from `./classActor`;
+import {Social} from `./Social`;
 
 
-let terminator = new classMovie ("Terminator", "1984", "107 minutes");
+let terminator = new classMovie (`Terminator`, `1984`, `107 minutes`);
   console.log(terminator.title);
   console.log(terminator.year);
   console.log(terminator.duration);
   terminator.play();
-  terminator.on("play", mylogger.log);
+  terminator.on(`play`, mylogger.log);
 
-let elCuerpo = new classMovie ("El Cuerpo", "2012", "106 minutes");
+let elCuerpo = new classMovie (`El Cuerpo`, `2012`, `106 minutes`);
   console.log(elCuerpo.title);
   console.log(elCuerpo.year);
   console.log(elCuerpo.duration);
   elCuerpo.play();
-  elCuerpo.on("play", mylogger.log);
+  elCuerpo.on(`play`, mylogger.log);
 
 Object.assign(terminator,Social);
 
 let Cast = [
-  new Actor("Arnold Schwarzenegger", 50);
-  new Actor("Paul Winfield", 50),
-  new Actor("Michael Biehn", 50),
-  new Actor("Linda Hamilton", 50)
+  new Actor(`Arnold Schwarzenegger`, 50);
+  new Actor(`Paul Winfield`, 50),
+  new Actor(`Michael Biehn`, 50),
+  new Actor(`Linda Hamilton`, 50)
 ];
 
 terminator.addCast(Cast);
-console.log("testing "+ ${terminator});
+console.log(`testing `+ ${terminator});
