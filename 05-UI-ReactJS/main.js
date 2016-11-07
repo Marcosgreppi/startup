2c938069dd410ba3c5b6e5bc4d9d1cca486142ae
Marcos Greppi <marcos.greppi@globant.com>
2016-11-07 17:47:42 -0300

    added ReatrRuter and Reflux

--- 05-UI-ReactJS/main.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Movies from "./Movies";
-import InputMovie from "./InputMovie";
-import ListFavMovies from "./ListFavMovies";
-
-ReactDOM.render(<Movie />, document.getElementById("container"));
--- 05-UI-ReactJS/source/index.js
+++ 05-UI-ReactJS/source/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, Link, browserHistory } from "react-router";
+import MovieApp from "./movieApp.js";
+import MovieList from "./movieList.js";
+import MovieForm from "./movieForm.js";
 
-let HelloWorld = React.createClass({
-    render: function () {
-        return (
-            <div className="hello-world">
-                <span>Hello World!</span>
-            </div>
-        );
-    }
-});
+class App extends React.Component {
+  render () {
+    return (
+      <div className="app">
+        <h1>Movies</h1>
+        <ul role="nav">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/movies">List</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="movies" component={MovieApp} />
+      <Route path="movie/:movieId" component={MovieForm} />
+    </Route>
+  </Router>
+), document.getElementById("app"));
